4def3eb745cd09e4f2a4c598c218ca357ca0a926
Timon0 <timon.kurmann@gmail.com>
2017-05-18 16:30:39 +0200

    Stationsnearby changed

--- SBB_Fahrplan_2.0/StationsNearby.Designer.cs
+++ SBB_Fahrplan_2.0/StationsNearby.Designer.cs
@@ -29,12 +29,10 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.longtitudeTextBox = new System.Windows.Forms.TextBox();
-            this.latitudeTextBox = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
             this.searchStationsButton = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.stationsDataGridView = new System.Windows.Forms.DataGridView();
+            this.stationSearch = new SBB_Fahrplan_2._0.StationSearch();
+            this.label1 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.stationsDataGridView)).BeginInit();
             this.SuspendLayout();
@@ -43,43 +41,15 @@
             // 
             this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.groupBox1.Controls.Add(this.longtitudeTextBox);
-            this.groupBox1.Controls.Add(this.latitudeTextBox);
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.searchStationsButton);
             this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.searchStationsButton);
+            this.groupBox1.Controls.Add(this.stationSearch);
             this.groupBox1.Location = new System.Drawing.Point(41, 29);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(1308, 221);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Query";
-            // 
-            // longtitudeTextBox
-            // 
-            this.longtitudeTextBox.Location = new System.Drawing.Point(432, 60);
-            this.longtitudeTextBox.Name = "longtitudeTextBox";
-            this.longtitudeTextBox.Size = new System.Drawing.Size(360, 26);
-            this.longtitudeTextBox.TabIndex = 6;
-            // 
-            // latitudeTextBox
-            // 
-            this.latitudeTextBox.Location = new System.Drawing.Point(11, 60);
-            this.latitudeTextBox.Name = "latitudeTextBox";
-            this.latitudeTextBox.Size = new System.Drawing.Size(360, 26);
-            this.latitudeTextBox.TabIndex = 5;
-            // 
-            // label2
-            // 
-            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(428, 36);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(80, 20);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Longitude";
             // 
             // searchStationsButton
             // 
@@ -92,18 +62,6 @@
             this.searchStationsButton.Text = "Search Stations";
             this.searchStationsButton.UseVisualStyleBackColor = true;
             this.searchStationsButton.Click += new System.EventHandler(this.searchStationsButton_Click);
-            // 
-            // label1
-            // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(7, 36);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(67, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Latitude";
             // 
             // stationsDataGridView
             // 
@@ -124,6 +82,24 @@
             this.stationsDataGridView.Size = new System.Drawing.Size(1308, 402);
             this.stationsDataGridView.TabIndex = 5;
             // 
+            // stationSearch
+            // 
+            this.stationSearch.BackColor = System.Drawing.Color.Transparent;
+            this.stationSearch.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.stationSearch.Location = new System.Drawing.Point(11, 69);
+            this.stationSearch.Name = "stationSearch";
+            this.stationSearch.Size = new System.Drawing.Size(423, 203);
+            this.stationSearch.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Place";
+            // 
             // StationsNearby
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
@@ -143,11 +119,9 @@
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.TextBox longtitudeTextBox;
-        private System.Windows.Forms.TextBox latitudeTextBox;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button searchStationsButton;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView stationsDataGridView;
+        private System.Windows.Forms.Label label1;
+        private StationSearch stationSearch;
     }
 }
--- SBB_Fahrplan_2.0/StationsNearby.cs
+++ SBB_Fahrplan_2.0/StationsNearby.cs
@@ -24,7 +24,7 @@ namespace SBB_Fahrplan_2._0
 
         private void searchStationsButton_Click(object sender, EventArgs e)
         {
-            if (!validateInput())
+            if (!stationSearch.isValidStation())
             {
                 return;
             }
@@ -32,41 +32,17 @@ namespace SBB_Fahrplan_2._0
             NumberFormatInfo numberFormatInfo = new NumberFormatInfo();
             numberFormatInfo.NumberDecimalSeparator = ".";
 
-            var stations = tranport.GetStationsNearby(latitude.ToString(numberFormatInfo), longtitude.ToString(numberFormatInfo));
+            var stations = tranport.GetStationsNearby(stationSearch.getStation().Coordinate.XCoordinate.ToString(numberFormatInfo), stationSearch.getStation().Coordinate.YCoordinate.ToString(numberFormatInfo));
             BindingList<StationsNearbyRow> stationsNearbyRows = new BindingList<StationsNearbyRow>();
             StationsNearbyConverter converter = new StationsNearbyConverter();
 
-            foreach(Station station in stations.StationList)
+            foreach (Station station in stations.StationList)
             {
                 stationsNearbyRows.Add(converter.toStationsNearbyRow(station));
             }
 
             stationsDataGridView.DataSource = stationsNearbyRows;
         }
-
-        private bool validateInput()
-        {
-            string errors = "";
-            NumberFormatInfo numberFormatInfo = new NumberFormatInfo();
-            numberFormatInfo.NumberDecimalSeparator = ".";
-
-            if (!Double.TryParse(longtitudeTextBox.Text, NumberStyles.Number, numberFormatInfo, out longtitude))
-            {
-                errors += "Longtitude value is invalid!\n";
-            }
-
-            if (!Double.TryParse(latitudeTextBox.Text, NumberStyles.Number, numberFormatInfo, out latitude))
-            {
-                errors += "Latitude value is invalid!\n";
-            }
-
-            if (errors.Length > 0)
-            {
-                MessageBox.Show(errors, "Validation error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return false;
-            }
-
-            return true;
-        }
+      
     }
 }
